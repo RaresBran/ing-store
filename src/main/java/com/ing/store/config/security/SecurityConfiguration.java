613ffdfe2b332baf07ce1923c9605461dc77d732
Rares <rares.bran@yahoo.ro>
2025-05-08 22:04:30 +0300

    Configure headers with defaults

--- src/main/java/com/ing/store/config/security/SecurityConfiguration.java
+++ src/main/java/com/ing/store/config/security/SecurityConfiguration.java
@@ -3,11 +3,11 @@ package com.ing.store.config.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -31,7 +31,12 @@ public class SecurityConfiguration {
         http
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-                .headers(HeadersConfigurer::disable)
+                .headers(headers -> headers
+                    .contentTypeOptions(Customizer.withDefaults())
+                    .xssProtection(Customizer.withDefaults())
+                    .cacheControl(Customizer.withDefaults())
+                    .httpStrictTransportSecurity(Customizer.withDefaults())
+                )
                 .authorizeHttpRequests(req -> req
                         .requestMatchers(WHITELIST).permitAll()
                         .anyRequest().authenticated()
